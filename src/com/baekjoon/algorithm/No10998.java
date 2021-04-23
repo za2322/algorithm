0bd5acc08cb518bafe415987893f5239996d2992
sbkim23 <za2322@naver.com>
2021-04-23 20:26:44 +0900

    bj: 10998

---
+++ src/com/baekjoon/algorithm/No10998.java
@@ -0,0 +1,16 @@
+package com.baekjoon.algorithm;
+
+import java.util.Scanner;
+
+// AXB 출력하기
+public class No10998 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 * num2);
+    }
+}
