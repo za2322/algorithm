0884f0cb280455fabee60728d59f0441d9051dcb
sbkim23 <za2322@naver.com>
2021-04-23 20:53:56 +0900

    bj: 10869

--- src/com/baekjoon/algorithm/No1000.java
+++ src/com/baekjoon/algorithm/No1000.java
@@ -6,13 +6,10 @@ import java.util.Scanner;
 public class No1000 {
     public static void main(String[] args) {
 
-        int num1 = 0;
-        int num2 = 0;
-
         Scanner sc = new Scanner(System.in);
 
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
         System.out.println(num1 + num2);
     }
--- src/com/baekjoon/algorithm/No10869.java
+++ src/com/baekjoon/algorithm/No10869.java
@@ -2,8 +2,8 @@ package com.baekjoon.algorithm;
 
 import java.util.Scanner;
 
-// A-B 출력하기
-public class No1001 {
+// 사칙연산
+public class No10869 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -11,6 +11,10 @@ public class No1001 {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
 
+        System.out.println(num1 + num2);
         System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
     }
 }
