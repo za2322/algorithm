cada74d97250288c80ee149fb63026ebbaeb180b
sbkim23 <za2322@naver.com>
2021-04-23 20:48:33 +0900

    bj: 1008

---
+++ src/com/baekjoon/algorithm/No1008.java
@@ -0,0 +1,16 @@
+package com.baekjoon.algorithm;
+
+import java.util.Scanner;
+
+// A/B 출력하기
+public class No1008 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+
+        System.out.println(num1 / num2);
+    }
+}
