94250a614dca986bf930a440f7aac27b1f42a352
sbkim23 <za2322@naver.com>
2021-04-24 13:07:08 +0900

    bj: 2750 insertion sort

---
+++ src/com/baekjoon/algorithm/No2750.java
@@ -0,0 +1,35 @@
+package com.baekjoon.algorithm;
+
+import java.util.Scanner;
+
+// 정렬 : 삽입정렬
+public class No2750 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int testNum = sc.nextInt();
+
+        int[] arr = new int[testNum];
+
+        for (int i = 0; i < testNum; i++) {
+            int a = sc.nextInt();
+
+            arr[i] = a;
+        }
+
+        for (int i = 0; i < testNum - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = tmp;
+                }
+            }
+        }
+
+        for (int i = 0; i < testNum; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
