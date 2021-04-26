6fc615b49693d6f922224cdb3adb22d179eb7eb9
sbkim23 <za2322@naver.com>
2021-04-26 12:22:40 +0900

    bj: 10815 binary search

---
+++ src/com/baekjoon/algorithm/binarysearch/No10815_BinarySearch.java
@@ -0,0 +1,62 @@
+package com.baekjoon.algorithm.binarysearch;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+// 정렬 : BinarySearch
+public class No10815_BinarySearch {
+
+    public static boolean binarySearch(ArrayList<Integer> array, int num) {
+        int start, end, mid;
+        start = 0;
+        end = array.size() - 1;
+        mid = (start + end) / 2;
+
+        while (end - start >= 0) {
+            if (array.get(mid) == num) {
+                return true;
+            }
+            else if (array.get(mid) < num) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+
+            mid = (start + end) / 2;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+
+        Collections.sort(arr);
+
+        int m = sc.nextInt();
+
+        ArrayList<Integer> testCase = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            testCase.add(sc.nextInt());
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (binarySearch(arr, testCase.get(i))) {
+                System.out.println(1);
+            }
+            else {
+                System.out.println(0);
+            }
+        }
+    }
+}
